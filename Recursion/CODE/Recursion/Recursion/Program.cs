fde0af63a698bc5fa12fa0158e18436d41012dc7
Camilo Andrés Rojas Hernández <93359519+Gaussito@users.noreply.github.com>
2022-11-27 12:07:46 -0500

    Recursions Update

--- Recursion/CODE/Recursion/Recursion/Program.cs
+++ Recursion/CODE/Recursion/Recursion/Program.cs
@@ -9,7 +9,8 @@ namespace Recursion
         static void Main(string[] args)
         {
             // IterationvsRecursion.Run();
-            TypeRecursion.Run();
+            // TypeRecursion.Run();
+            Exercises.Run();
         }
     }
 }
---
+++ Recursion/CODE/Recursion/Recursion/Repo/Exercises.cs
@@ -0,0 +1,17 @@
+﻿namespace Recursion.Repo;
+
+public class Exercises
+{
+    public int SumOfN(int n)
+    {
+        if (n == 1) return 1;
+        return n + SumOfN(n - 1);
+    }
+
+    public static void Run()
+    {
+        var exercises = new Exercises();
+        Console.WriteLine("Sum of N");
+        Console.WriteLine(exercises.SumOfN(5));
+    }
+}
